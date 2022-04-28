5bb785e61fd31e8fb5d732f679491df57eb40fa0
Anastasia Kutsevolova <anastasiasemenok@gmail.com>
2022-04-27 21:13:02 -0600

    added name fields and transformed response

--- models/user.js
+++ models/user.js
@@ -1,15 +1,32 @@
 const mongoose = require("mongoose");
 
 //User Schema
-const userSchema = mongoose.Schema({
-  email: {
-    type: String,
-    required: [true, "Email is required"],
+const userSchema = mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
   },
-  password: {
-    type: String,
-    required: [true, "Password is required"],
-  },
-});
+  {
+    toJSON: {
+      transform(doc, ret) {
+        ret.id = ret._id;
+        delete ret._id;
+        delete ret.password;
+        delete ret.__v;
+      },
+    },
+  }
+);
 
 const User = (module.exports = mongoose.model("User", userSchema));
