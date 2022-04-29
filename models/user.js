639c9bbfe05155cb0ddbf28e512d1f5e3bdc9795
Anastasia Kutsevolova <anastasiasemenok@gmail.com>
2022-04-28 20:55:26 -0600

    property changes for userSchema

--- models/user.js
+++ models/user.js
@@ -11,6 +11,7 @@ const userSchema = mongoose.Schema(
       type: String,
       required: true,
       unique: true,
+      set: (value) => value.toLowerCase(),
     },
     password: {
       type: String,
