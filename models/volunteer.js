0a24f2a5388292958a1afbd00421f68fd6b02d1f
Anastasia Kutsevolova <anastasiasemenok@gmail.com>
2022-05-05 19:32:23 -0600

    added author

--- models/volunteer.js
+++ models/volunteer.js
@@ -1,25 +1,40 @@
 const mongoose = require("mongoose");
 
 //Volunteer Schema
-const volunteerSchema = new mongoose.Schema({
-  name: {
-    type: String,
-    required: true,
-    min: 2,
+const volunteerSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+      min: 2,
+    },
+    email: {
+      type: String,
+      required: true,
+    },
+    about: {
+      type: String,
+      required: true,
+      max: 300,
+    },
+    createdAt: {
+      type: Date,
+      default: Date.now,
+    },
+    author: {
+      type: String,
+      required: true,
+    },
   },
-  email: {
-    type: String,
-    required: true,
-  },
-  about: {
-    type: String,
-    required: true,
-    max: 300,
-  },
-  createdAt: {
-    type: Date,
-    default: Date.now,
-  },
-});
+  {
+    toJSON: {
+      transform(_, ret) {
+        ret.id = ret._id;
+        delete ret._id;
+        delete ret.__v;
+      },
+    },
+  }
+);
 
 module.exports = mongoose.model("Volunteer", volunteerSchema);
