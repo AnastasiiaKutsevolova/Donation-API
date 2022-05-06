81885660714f093c7987e82e01f827f125fdfddf
Anastasia Kutsevolova <anastasiasemenok@gmail.com>
2022-05-05 19:31:59 -0600

    improvements

--- routes/user.js
+++ routes/user.js
@@ -1,13 +1,13 @@
 const { Router } = require("express");
 const User = require("../models/user");
 const auth = require("../middleware/auth.middleware");
-const { route } = require("./auth");
+const { sanitizedObj } = require("../utils");
 
 const router = Router();
 // Find user
-router.get("/user/:id", auth, async (req, res) => {
+router.get("/user", auth, async (req, res) => {
   try {
-    const user = await User.findById(req.params.id);
+    const user = await User.findById(req.user.userId);
 
     if (!user) {
       return res.status(400).json({ message: "User not found!" });
@@ -20,7 +20,7 @@ router.get("/user/:id", auth, async (req, res) => {
 });
 
 // User update
-router.put("/user/:id", auth, async (req, res) => {
+router.put("/user", auth, async (req, res) => {
   try {
     const { name, password } = req.body;
 
@@ -29,15 +29,10 @@ router.put("/user/:id", auth, async (req, res) => {
       password,
     };
 
-    // Filtering empty values
-    const sanitizedUser = Object.keys(user).reduce(
-      (acc, key) => (user[key] ? { ...acc, [key]: user[key] } : acc),
-      {}
-    );
-
     const updatedUser = await User.findByIdAndUpdate(
-      req.params.id,
-      sanitizedUser
+      req.user.userId,
+      sanitizedObj(user),
+      { new: true } // returns the new document
     );
 
     res.status(200).json({ user: updatedUser });
@@ -49,6 +44,7 @@ router.put("/user/:id", auth, async (req, res) => {
 // Delete User
 router.delete("/user/:id", auth, async (req, res) => {
   try {
+    // TODO remove associated posts
     await User.findByIdAndDelete(req.params.id);
 
     res.status(204).send();
